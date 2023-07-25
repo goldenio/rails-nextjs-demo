0acd31619c79e6c30ad901c824fed5598093eea6
sean <seanwang@goldenio.com>
2023-07-25 17:26:23 +0800

    add db:seed

--- oauth_provider/db/seeds.rb
+++ oauth_provider/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+manager_email = 'manager@golden.io'
+manager = Manager.create_with(
+  password: manager_email
+).find_or_create_by(email: manager_email)
+
+user_email = 'user@golden.io'
+user = User.create_with(
+  password: user_email
+).find_or_create_by(email: user_email)
